6afdc9fb9dc7749c26594f70b0f6fb0227d9eeac
Choi Vlad <dev.choivlad@gmail.com>
2025-01-11 11:55:57 -0800

    passing custom params to event function

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 import TabButton from "./components/TabButton";
+import {Component} from "react";
 
 function App() {
-  function handleSelect() {
-    console.log("the tab button is selected");
+  function handleSelect(selectedBtn) {
+    // seceltedBtn => "components", "jsx", "props", "state"
+    console.log(selectedBtn);
   }
   return (
     <div>
@@ -29,10 +31,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
         </section>
       </main>
