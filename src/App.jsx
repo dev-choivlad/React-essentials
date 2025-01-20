d4ca426515083e89b8a46dea0811641d7d99cf89
Choi Vlad <dev.choivlad@gmail.com>
2025-01-19 18:01:24 -0800

    outputting data list dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -34,15 +34,18 @@ function App() {
       <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
+            {CORE_CONCEPTS.map((conceptItem) =>
+              (<CoreConcept key={conceptItem.title} {...conceptItem} />)
+            )}
+
+            {/*title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}/>
-            {/*если props names одинаковы с названиями ключей в импортируемом объекте CORE_CONCEPTS
-            можно использовать spread operator для того, чтобы вытянуть все значения*/}
+            {/!*если props names одинаковы с названиями ключей в импортируемом объекте CORE_CONCEPTS
+            можно использовать spread operator для того, чтобы вытянуть все значения*!/}
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>*/}
           </ul>
         </section>
 
